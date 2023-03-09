e1079a50d2d82d771d99c035c6e484f443da774a
slasher125 <slasher125@protonmail.com>
2023-03-09 15:38:42 +0400

    add roles

--- migrations/1678347951206_db-schema.js
+++ migrations/1678347951206_db-schema.js
@@ -1,5 +1,6 @@
 // migration file using node-pg-migrate
 exports.up = (pgm) => {
+  // collection table for static data
   pgm.sql(`
       CREATE TABLE collection (
         collection_id VARCHAR(42) PRIMARY KEY,
@@ -12,6 +13,7 @@ exports.up = (pgm) => {
       );
     `);
 
+  // floor table for timeseries data
   pgm.sql(`
       CREATE TABLE floor (
         floor_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -26,6 +28,7 @@ exports.up = (pgm) => {
       );
     `);
 
+  // function
   pgm.sql(`
       CREATE OR REPLACE FUNCTION update_updated_at()
       RETURNS TRIGGER AS $$
@@ -36,10 +39,40 @@ exports.up = (pgm) => {
       $$ LANGUAGE plpgsql;
     `);
 
+  // trigger
   pgm.sql(`
       CREATE TRIGGER update_updated_at
       BEFORE UPDATE ON collection
       FOR EACH ROW
       EXECUTE FUNCTION update_updated_at();
+    `);
+
+  // composite index on timeseries table
+  pgm.sql(`
+      CREATE INDEX floor_collection_id_timestamp_idx 
+      ON yield (collection_id ASC, timestamp DESC);
+  `);
+
+  // roles
+  pgm.sql(`
+      -- Revoke privileges from 'public' role
+      REVOKE CREATE ON SCHEMA public FROM PUBLIC;
+      REVOKE ALL ON DATABASE nft FROM PUBLIC;
+      
+      -- Read-only role
+      CREATE ROLE readonly;
+      GRANT CONNECT ON DATABASE nft TO readonly;
+      GRANT USAGE ON SCHEMA public TO readonly;
+      GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
+      ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly;
+      
+      -- Read/write role
+      CREATE ROLE readwrite;
+      GRANT CONNECT ON DATABASE nft TO readwrite;
+      GRANT USAGE ON SCHEMA public TO readwrite;
+      GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO readwrite;
+      ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO readwrite;
+      GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO readwrite;
+      ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE ON SEQUENCES TO readwrite;
     `);
 };
