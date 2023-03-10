b89d205cae6e74ddc097bc5c84b94344d47b13dd
slasher125 <slasher125@protonmail.com>
2023-03-10 13:33:16 +0400

    add floor price pct change to query

--- src/controllers/collections.js
+++ src/controllers/collections.js
@@ -101,7 +101,37 @@ SELECT
     floor_price,
     floor_price_1_day,
     floor_price_7_day,
-    floor_price_30_day
+    floor_price_30_day,
+    CASE
+        WHEN floor_price_1_day IS NULL
+        OR floor_price_1_day = 0 THEN NULL
+        ELSE ROUND(
+            (
+                (floor_price - floor_price_1_day) / floor_price_1_day
+            ) * 100,
+            5
+        )
+    END AS floor_price_change_1_day,
+    CASE
+        WHEN floor_price_7_day IS NULL
+        OR floor_price_7_day = 0 THEN NULL
+        ELSE ROUND(
+            (
+                (floor_price - floor_price_7_day) / floor_price_7_day
+            ) * 100,
+            5
+        )
+    END AS floor_price_change_7_day,
+    CASE
+        WHEN floor_price_30_day IS NULL
+        OR floor_price_30_day = 0 THEN NULL
+        ELSE ROUND(
+            (
+                (floor_price - floor_price_30_day) / floor_price_30_day
+            ) * 100,
+            5
+        )
+    END AS floor_price_change_30_day
 FROM
     filtered_records AS f
     INNER JOIN $<collectionTable:name> AS c ON c.collection_id = f.collection_id;
