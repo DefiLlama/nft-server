8ea7e9a3931a1142f2edb5bbb7984c93507fc2b5
slasher125 <slasher125@protonmail.com>
2023-03-10 13:00:06 +0400

    fix artblocks

--- src/handlers/triggerCollections.js
+++ src/handlers/triggerCollections.js
@@ -14,25 +14,34 @@ module.exports.handler = async () => {
 const main = async () => {
   console.log('trigger Collections handler...\n');
   const api = 'https://api.reservoir.tools';
+
+  const apiKey = {
+    headers: { 'x-api-key': process.env.RESERVOIR_API },
+  };
+
   // get top 1k collections based on all time volume
   // note: collections/v1 'only' inlcudes floor price but no floor price history/totalSupply etc
   // which is why we call /collections/v5.
-  // we use collections/v1 mainly to obtain a list of top N collections based sorted by all time trade volume
-  const top1k = (await axios.get(`${api}/search/collections/v1?limit=1000`))
-    .data.collections;
+  // we use collections/v1 mainly to obtain a list of top N collections sorted by all time trade volume
+  const top1k = (
+    await axios.get(`${api}/search/collections/v1?limit=1000`, apiKey)
+  ).data.collections;
+  await sleep(1000);
+
+  // remove artblocks (requires separate call cause contract identical between arbtlocks nfts which breaks the
+  // the following api calls)
+  const exArtBlocks = top1k.filter((c) => c.collectionId.length <= 42);
+  const artBlocksInTop1k = top1k.length - exArtBlocks.length;
+
   // get the collection addresses
-  const ids = top1k.map((c) => c.collectionId.slice(0, 42));
+  const ids = exArtBlocks.map((c) => c.collectionId);
 
   // collections/v5 accepts a max of 20 collection ids per request
   const size = 20;
   const requests = [];
   for (let i = 0; i < ids.length; i += size) {
     const batch = ids.slice(i, size + i).join('&contract=');
-    requests.push(
-      axios.get(`${api}/collections/v5?contract=${batch}`, {
-        headers: { 'x-api-key': process.env.RESERVOIR_API },
-      })
-    );
+    requests.push(axios.get(`${api}/collections/v5?contract=${batch}`, apiKey));
   }
 
   // free api key rate limite = 4RPS
@@ -48,7 +57,21 @@ const main = async () => {
     await sleep(1000);
   }
 
-  console.log('nb of failed calls:', ids.length - collectionDetails.length);
+  // get artblocks
+  const calls = Math.ceil(artBlocksInTop1k / size);
+  const artblocksUrl = `${api}/collections/v5?community=artblocks`;
+
+  let continuation;
+  let collections;
+  for (let i = 0; i < calls; i++) {
+    const url =
+      i === 0 ? artblocksUrl : `${artblocksUrl}&continuation=${continuation}`;
+
+    ({ collections, continuation } = (await axios.get(url, apiKey)).data);
+    await sleep(1000);
+
+    collectionDetails = [...collectionDetails, ...collections];
+  }
 
   const timestamp = new Date(Date.now());
   const seen = new Set();
@@ -74,6 +97,8 @@ const main = async () => {
     );
     seen.add(c.id);
   }
+
+  console.log(collectionDetails.length, payload.length);
 
   // db insert
   console.log('insert collections...');
