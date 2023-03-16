97a5b86014c69b325fd2385f0714e7b24d52494a
slasher125 <slasher125@protonmail.com>
2023-03-16 17:46:50 +0400

    add api endpoints

--- env.js
+++ env.js
@@ -4,5 +4,6 @@ try {
 
 module.exports = {
   DATABASE_URL_NFT: process.env.DATABASE_URL_NFT,
+  DATABASE_URL_INDEXA: process.env.DATABASE_URL_INDEXA,
   RESERVOIR_API: process.env.RESERVOIR_API,
 };
