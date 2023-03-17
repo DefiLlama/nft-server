eb7d6109dd66bcaf67134ad750cfd1e5197aaf3c
slasher125 <slasher125@protonmail.com>
2023-03-17 18:04:43 +0400

    other aggregators

--- src/adapters/aggregators.js
+++ src/adapters/aggregators.js
@@ -17,4 +17,92 @@ module.exports = [
       '0x0000000035634b55f3d99b071b5a354f48e10bef',
     ],
   },
+  {
+    name: 'Genie',
+    contracts: [
+      '0xcdface5643b90ca4b3160dd2b5de80c1bf1cb088',
+      '0x31837aaf36961274a04b915697fdfca1af31a0c7',
+      '0xf97e9727d8e7db7aa8f006d1742d107cf9411412',
+      '0x2af4b707e1dce8fc345f38cfeeaa2421e54976d5',
+      '0x0a267cf51ef038fc00e71801f5a524aec06e4f07',
+    ],
+  },
+  {
+    name: 'Uniswap',
+    contracts: ['0xef1c6e67703c7bd7107eed8303fbe6ec2554bf6b'],
+  },
+  {
+    name: 'Reservoir',
+    contracts: [
+      '0x9ebfb53fa8526906738856848a27cb11b0285c3f',
+      '0x8005488ff4f8982d2d8c1d602e6d747b1428dd41',
+      '0x178a86d36d89c7fdebea90b739605da7b131ff6a',
+      '0x5aa9ca240174a54af6d9bfc69214b2ed948de86d',
+      '0xc52b521b284792498c1036d4c2ed4b73387b3859',
+      '0x7c9733b19e14f37aca367fbd78922c098c55c874',
+    ],
+  },
+  {
+    name: 'Alpha Sharks',
+    contracts: [
+      '0x552b16d19dbad7af2786fe5a40d96d2a5c09428c',
+      '0x114e54a100a0415abf9727234c92c83dbcc59abf',
+      '0x141efc30c4093bc0f8204accb8afa6643fddecf2',
+      '0x04898894a0b6c094a920eafd180ef4ac30f00a43',
+      '0x1ed3d33b41e392014e0c9d8125369aba4e09798f',
+    ],
+  },
+  { name: 'X2Y2', contracts: ['0x56dd5bbede9bfdb10a2845c4d70d4a2950163044'] },
+  {
+    name: 'Element',
+    contracts: [
+      '0x69cf8871f61fb03f540bc519dd1f1d4682ea0bf6',
+      '0xb4e7b8946fa2b35912cc0581772cccd69a33000c',
+    ],
+  },
+  {
+    name: 'Rarity Garden',
+    contracts: [
+      '0x9d0a89bc35fb160a076de0341d9280830d3013ca',
+      '0x39b6862c4783db2651d64bc160349dc9a15f1fb7',
+    ],
+  },
+  {
+    name: 'BitKeep',
+    contracts: ['0x603d022611bfe6a101dcdab207d96c527f1d4d8e'],
+  },
+  {
+    name: 'Flip',
+    contracts: [
+      '0x36ab1c395b3711d3d5ed2af8ac8371cc991aa06c',
+      '0x7db11e30ae8ad7495668701c3f2c1b6d60587eda',
+    ],
+  },
+  {
+    name: 'Rarible',
+    contracts: [
+      '0x2a7251d1e7d708c507b1b0d3ff328007beecce5d',
+      '0x2c45af926d5f62c5935278106800a03eb565778e',
+    ],
+  },
+  {
+    name: 'Magic Eden',
+    contracts: ['0x1ee3151cff01321059e3865214379b85c79ca984'],
+  },
+  { name: 'OKX', contracts: ['0x92701d42e1504ef9fce6d66a2054218b048dda43'] },
+  {
+    name: 'GigaMart',
+    contracts: [
+      '0x4c9712cd94376c537464caa4d87bce198d59936c',
+      '0xdc077a4e3f46138dedac5d684882d33fcc927cf7',
+    ],
+  },
+  {
+    name: 'Skillet',
+    contracts: ['0x4abab19c2ad968adbc52e2c8a5ccda5379629576'],
+  },
+  {
+    name: 'Tiny Astro',
+    contracts: ['0xb123504fa220ba482768dd1e798594c1af88d7dc'],
+  },
 ];
