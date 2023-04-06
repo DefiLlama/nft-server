07011c5d8135c6cf45d80d4c38eb391558b29ee8
slasher125 <slasher125@protonmail.com>
2023-04-06 20:09:27 +0400

    random sample if more than 30k samples

--- src/controllers/nftTrades.js
+++ src/controllers/nftTrades.js
@@ -232,25 +232,8 @@ const getSalesLiteX = async (collectionId) => {
     [collectionId, lb, ub] = collectionId.split(':');
   }
 
-  // const query = minify(`
-  //   SELECT
-  //       DISTINCT ON (date_trunc('min', block_time))
-  //       EXTRACT(EPOCH FROM block_time) AS block_time,
-  //       eth_sale_price
-  //   FROM
-  //       ethereum.nft_trades_clean
-  //   WHERE
-  //       collection = $<collectionId>
-  //       ${
-  //         lb
-  //           ? "AND encode(token_id, 'escape')::numeric BETWEEN $<lb> AND $<ub>"
-  //           : ''
-  //       }
-  //   ORDER BY date_trunc('min', block_time)
-  // `);
-
   const query = minify(`
-  SELECT
+  WITH sales AS (SELECT
       EXTRACT(EPOCH FROM block_time) AS block_time,
       eth_sale_price
   FROM
@@ -262,7 +245,12 @@ const getSalesLiteX = async (collectionId) => {
           ? "AND encode(token_id, 'escape')::numeric BETWEEN $<lb> AND $<ub>"
           : ''
       }
-      AND RANDOM() <= 0.5;
+  )
+  SELECT *
+  FROM
+      sales
+  WHERE
+      (SELECT COUNT(*) FROM sales) <= 30000 OR RANDOM() <= 0.5;
 `);
 
   const response = await conn.query(query, {
