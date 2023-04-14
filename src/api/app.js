ceac42e105304e258125f927ec7f29add738d60c
slasher125 <slasher125@protonmail.com>
2023-04-14 15:30:15 +0400

    logging

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const trades = require('./routes/trades');
 const floor = require('./routes/floor');
 
 const app = express();
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(helmet());
 app.use(cors());
 
