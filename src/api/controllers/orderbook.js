e3aa6b9f8622890239e17155d7f56bd89a7b3727
slasher125 <slasher125@protonmail.com>
2023-04-19 10:44:46 +0400

    asks summary stats

--- src/api/controllers/orderbook.js
+++ src/api/controllers/orderbook.js
@@ -12,21 +12,29 @@ const getOrders = async (req, res) => {
 
   const query = minify(
     `
-SELECT
+WITH asks AS (SELECT
     price, amount
 FROM
     orderbook
 WHERE
-	collection_id = $<collectionId>
-	AND timestamp = (
+  collection_id = $<collectionId>
+  AND timestamp = (
         SELECT
             max(timestamp)
         FROM
             orderbook
         WHERE
-            collection_id = $<collectionId>
+            collection_id =  $<collectionId>
             AND timestamp >= NOW() - INTERVAL '1 day'
             )
+ORDER BY price
+)
+SELECT
+  price,
+  SUM(price) OVER (ORDER BY price) AS price_total,
+  AVG(price) OVER (ORDER BY price ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS avg_price,
+  SUM(amount) OVER (ORDER BY price) AS amount
+FROM asks
     `
   );
 
