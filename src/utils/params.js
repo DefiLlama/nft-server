9755a0d269a95ce8d06d0e1ca243f720b05cc557
slasher125 <slasher125@protonmail.com>
2023-05-03 14:56:58 +0400

    looksrare v1 roy

--- src/utils/params.js
+++ src/utils/params.js
@@ -7,5 +7,7 @@ module.exports = {
       'ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
     erc1155_TransferSingle:
       'c3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62',
+    looksrareV1RoyaltyPayment:
+      '27c4f0403323142b599832f26acd21c74a9e5b809f2215726e244a4ac588cd7d',
   },
 };
