a018447b924a5ded11177628208401dfd5b5235b
slasher125 <slasher125@protonmail.com>
2023-05-03 20:56:44 +0400

    add royalty endpoint

--- src/api/controllers/trades.js
+++ src/api/controllers/trades.js
@@ -353,10 +353,44 @@ const getExchangeVolume = async (req, res) => {
     );
 };
 
+const getRoyalties = async (req, res) => {
+  let collectionId = req.params.collectionId;
+  if (!checkCollection(collectionId))
+    return res.status(400).json('invalid collectionId!');
+
+  const query = minify(`
+SELECT
+    SUM(CASE WHEN block_time >= (NOW() - INTERVAL '1 DAY') THEN royalty_fee_eth END) AS "royalties_eth_1d",
+    SUM(CASE WHEN block_time >= (NOW() - INTERVAL '7 DAY') THEN royalty_fee_eth END) AS "royalties_eth_7d",
+    SUM(CASE WHEN block_time >= (NOW() - INTERVAL '30 DAY') THEN royalty_fee_eth END) AS "royalties_eth_30d",
+    SUM(CASE WHEN block_time >= (NOW() - INTERVAL '1 DAY') THEN royalty_fee_usd END) AS "royalties_usd_1d",
+    SUM(CASE WHEN block_time >= (NOW() - INTERVAL '7 DAY') THEN royalty_fee_usd END) AS "royalties_usd_7d",
+    SUM(CASE WHEN block_time >= (NOW() - INTERVAL '30 DAY') THEN royalty_fee_usd END) AS "royalties_usd_30d"
+FROM
+    ethereum.nft_trades
+WHERE
+    collection = $<collectionId>
+`);
+
+  const response = await indexa.query(query, {
+    collectionId: `\\${collectionId.slice(1)}`,
+  });
+
+  if (!response) {
+    return new Error(`Couldn't get data`, 404);
+  }
+
+  res
+    .set(customHeader())
+    .status(200)
+    .json(response.map((c) => convertKeysToCamelCase(c)));
+};
+
 module.exports = {
   getSales,
   getStats,
   getVolume,
   getExchangeStats,
   getExchangeVolume,
+  getRoyalties,
 };
