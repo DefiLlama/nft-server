e862ed8a3268bee277aa11ada9ceb2e6ce36547c
slasher125 <slasher125@protonmail.com>
2023-05-15 17:24:32 +0400

    wyvern fix

--- src/adapters/openseaWyvern/index.js
+++ src/adapters/openseaWyvern/index.js
@@ -1,48 +1,88 @@
+const { stripZerosLeft } = require('ethers');
+
 const abi = require('./abi.json');
 const config = require('./config.json');
 const { nftTransferEvents } = require('../../utils/params');
+const getHistoricalTokenPrice = require('../../utils/price');
 
 const parse = async (decodedData, event, events) => {
-  // remove potential lazy mint and match on transaction_hash
   const transfers = events.filter(
+    (e) => e.transaction_hash === event.transaction_hash
+  );
+
+  // erc20 transfers
+  const transfersERC20 = transfers.filter(
     (e) =>
-      e.topic_1 !==
-        '0000000000000000000000000000000000000000000000000000000000000000' &&
-      e.transaction_hash === event.transaction_hash
+      e.topic_0 === nftTransferEvents['erc721_Transfer'] && e.topic_3 === null
+  );
+  const transfersERC20LogIndices = transfersERC20.map((i) => i.log_index);
+
+  // erc721/erc1155 transfers
+  const transfersNFT = transfers.filter(
+    (e) => !transfersERC20LogIndices.includes(e.log_index)
   );
 
   // find the first transfer event which has the from address in either topic_1 - topic_3
   // so we catch both cases of erc721 transfer and erc1155 TransferSingle
-  const transferEvent = transfers.find(
+  const transferEventNFT = transfersNFT.find(
     (tf) =>
       tf.topic_1.includes(event.from_address) ||
       tf.topic_2.includes(event.from_address) ||
       tf.topic_3.includes(event.from_address)
   );
 
-  if (!transferEvent) return {};
+  if (!transferEventNFT) return {};
 
   let tokenId;
-  if (transferEvent.topic_0 === nftTransferEvents['erc721_Transfer']) {
-    tokenId = BigInt(`0x${transferEvent.topic_3}`);
+  if (transferEventNFT.topic_0 === nftTransferEvents['erc721_Transfer']) {
+    tokenId = BigInt(`0x${transferEventNFT.topic_3}`);
   } else if (
-    transferEvent.topic_0 === nftTransferEvents['erc1155_TransferSingle']
+    transferEventNFT.topic_0 === nftTransferEvents['erc1155_TransferSingle']
   ) {
-    tokenId = BigInt(`0x${transferEvent.data.slice(0, 64)}`);
+    tokenId = BigInt(`0x${transferEventNFT.data.slice(0, 64)}`);
   }
 
   const { maker, taker, price } = decodedData;
 
-  const salePrice = price.toString() / 1e18;
+  let paymentToken;
+  let salePrice;
+  let ethSalePrice;
+  let usdSalePrice;
+
+  const nullAddress = '0x0000000000000000000000000000000000000000';
+  if (transfersERC20.length) {
+    const x = transfersERC20.find((t) => {
+      const d = stripZerosLeft(`0x${t.data}`);
+      return BigInt(d === '0x' ? '0x0' : d) === price;
+    });
+    // if eth transfer (x === undefined) or weth payment token
+    if (
+      x === undefined ||
+      x.contract_address === 'c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+    ) {
+      salePrice = ethSalePrice = price.toString() / 1e18;
+      paymentToken = x?.contract_address ?? nullAddress;
+      usdSalePrice = salePrice * event.price;
+    } else {
+      paymentToken = x.contract_address;
+
+      ({ salePrice, ethSalePrice, usdSalePrice, tokenPriceUsd } =
+        await getHistoricalTokenPrice(event, paymentToken, price));
+    }
+  } else {
+    salePrice = ethSalePrice = price.toString() / 1e18;
+    paymentToken = nullAddress;
+    usdSalePrice = salePrice * event.price;
+  }
 
   return {
-    collection: transferEvent.contract_address,
+    collection: transferEventNFT.contract_address,
     tokenId,
     amount: 1,
     salePrice,
-    ethSalePrice: salePrice,
-    usdSalePrice: salePrice * event.price,
-    paymentToken: '0x0000000000000000000000000000000000000000',
+    ethSalePrice,
+    usdSalePrice,
+    paymentToken,
     seller: maker,
     buyer: taker,
   };
--- src/adapters/queries.js
+++ src/adapters/queries.js
@@ -31,8 +31,6 @@ WHERE
   AND e.block_number <= $<endBlock>
 `;
 
-// opensea-wyvern & rarible events do not contain collection and tokenId, hence we need to also
-// query nft transfer events
 const queryExtensive = `
 SELECT
     encode(e.transaction_hash, 'hex') AS transaction_hash,
@@ -69,6 +67,62 @@ WHERE
         OR (
           e.topic_0 = '\\xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62'
           AND topic_1 != '\\x0000000000000000000000000000000000000000000000000000000000000000'
+      )
+    )
+    AND e.block_number >= $<startBlock>
+    AND e.block_number <= $<endBlock>
+    AND e.transaction_hash IN (
+	    SELECT
+            e.transaction_hash
+	    FROM
+            ethereum.event_logs e
+      WHERE
+            e.contract_address IN ($<contractAddresses:csv>)
+            AND e.topic_0 IN ($<eventSignatureHashes:csv>)
+            AND e.block_number >= $<startBlock>
+            AND e.block_number <= $<endBlock>
+	    )
+`;
+
+const queryWyvern = `
+SELECT
+    encode(e.transaction_hash, 'hex') AS transaction_hash,
+    e.log_index,
+    encode(e.contract_address, 'hex') AS contract_address,
+    encode(e.topic_0, 'hex') AS topic_0,
+    encode(e.topic_1, 'hex') AS topic_1,
+    encode(e.topic_2, 'hex') AS topic_2,
+    encode(e.topic_3, 'hex') AS topic_3,
+    encode(e.data, 'hex') AS data,
+    e.block_time,
+    e.block_number,
+    encode(e.block_hash, 'hex') AS block_hash,
+    b.price,
+    encode(t.from_address, 'hex') AS from_address,
+    encode(t.to_address, 'hex') AS to_address,
+    a.name AS aggregator_name
+FROM
+    ethereum.event_logs e
+    LEFT JOIN ethereum.blocks b ON e.block_time = b.time
+    LEFT JOIN ethereum.transactions t ON e.transaction_hash = t.hash
+    LEFT JOIN ethereum.nft_aggregators_appendage a ON RIGHT(encode(t.data, 'hex'), a.appendage_length) = encode(a.appendage, 'escape')
+WHERE
+    (
+        (
+            e.contract_address IN ($<contractAddresses:csv>)
+            AND e.topic_0 IN ($<eventSignatureHashes:csv>)
+        )
+        OR (
+            e.topic_0 = '\\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+            AND topic_1 != '\\x0000000000000000000000000000000000000000000000000000000000000000'
+            AND topic_3 IS NOT NULL
+        )
+        OR (
+          e.topic_0 = '\\xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62'
+          AND topic_1 != '\\x0000000000000000000000000000000000000000000000000000000000000000'
+      ) OR (
+        e.topic_0 = '\\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+        AND topic_3 IS NULL
       )
     )
     AND e.block_number >= $<startBlock>
@@ -174,15 +228,15 @@ const getEvents = async (task, startBlock, endBlock, config) => {
   );
   const contractAddresses = config.contracts.map((c) => `\\${c.slice(1)}`);
 
-  const q =
-    ['rarible', 'zora'].includes(config.exchangeName) ||
-    config.version === 'wyvern'
-      ? queryExtensive
-      : config.exchangeName === 'cryptopunks'
-      ? queryCryptopunks
-      : config.version === 'looksrare-v1'
-      ? queryLooksrareV1
-      : query;
+  const q = ['rarible', 'zora'].includes(config.exchangeName)
+    ? queryExtensive
+    : config.version === 'wyvern'
+    ? queryWyvern
+    : config.exchangeName === 'cryptopunks'
+    ? queryCryptopunks
+    : config.version === 'looksrare-v1'
+    ? queryLooksrareV1
+    : query;
 
   const response = await task.query(minify(q, { compress: false }), {
     startBlock,
