b0b6de76363949f9321cc7c1bf0c0b342281118a
slasher125 <slasher125@protonmail.com>
2023-05-15 17:54:11 +0400

    add columns to nft_collections table

--- src/tables/nft_collections.sql
+++ src/tables/nft_collections.sql
@@ -1,3 +1,8 @@
-CREATE TABLE IF NOT EXISTS ethereum.nft_collections (collection bytea, token_standard bytea);
+CREATE TABLE IF NOT EXISTS ethereum.nft_collections (
+    collection bytea,
+    token_standard bytea,
+    royalty_recipient bytea,
+    royalty_fee_pct numeric,
+);
 
 CREATE INDEX IF NOT EXISTS ethereum_nft_collections_collection_idx ON ethereum.nft_collections (collection);
