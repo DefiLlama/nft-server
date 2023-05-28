ece90c0d021b8a57b9ed083d19c968db0559dca8
slasher125 <slasher125@protonmail.com>
2023-05-28 09:27:17 +0400

    split monitor jobs

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,23 +1,16 @@
 const floor = require('./floor');
-const monitor = require('./monitor');
 const washTrades = require('./washTrades');
 const orderbook = require('./orderbook');
 const trimOrderbook = require('./trimOrderbook');
 const symbol = require('./symbol');
 const { tokenStandard } = require('./tokenStandard');
 const exchangeVolume = require('./exchangeVolume');
-const monitorFloor = require('./monitorFloor');
 
 const startJobs = () => {
   setInterval(async () => {
     console.log(`running floor job ${new Date()}...`);
     await floor();
   }, 60 * 60 * 1000);
-
-  setInterval(async () => {
-    console.log(`running monitor job ${new Date()}...`);
-    await monitor();
-  }, 15 * 60 * 1000);
 
   setInterval(async () => {
     console.log(`running washTrades job ${new Date()}...`);
@@ -48,11 +41,6 @@ const startJobs = () => {
     console.log(`running exchangeVolume job ${new Date()}...`);
     await exchangeVolume();
   }, 24 * 60 * 60 * 1000);
-
-  setInterval(async () => {
-    console.log(`running monitorFloor job ${new Date()}...`);
-    await monitorFloor();
-  }, 60 * 60 * 1000);
 };
 
 startJobs();
