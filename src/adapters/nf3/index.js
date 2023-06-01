ff7eb16ee30ea3c76e5ef2dc8cff42d48b3652e2
slasher125 <slasher125@protonmail.com>
2023-06-01 15:32:01 +0400

    add parse

--- src/adapters/nf3/index.js
+++ src/adapters/nf3/index.js
@@ -1,14 +1,57 @@
 const abi = require('./abi.json');
 const config = require('./config.json');
+const getHistoricalTokenPrice = require('../../utils/price');
 
-const parse = (decodedData, event) => {
+const ethPaymentTokens = [
+  '0x0000000000000000000000000000000000000000',
+  '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+];
+
+const parse = async (decodedData, event) => {
   const {
-    listing: { listingAssets: tokens },
+    listing: {
+      owner,
+      tradeIntendedFor,
+      listingAssets: { paymentTokens, amounts },
+    },
+    offeredAssets: { tokens, tokenIds },
   } = decodedData;
 
-  const collection = tokens[0][0];
-  const tokenId = tokens[1][0];
-  console.log(collection, tokenId);
+  const collection = tokens[0];
+  const tokenId = tokenIds[0];
+  const seller = tradeIntendedFor;
+  const buyer = owner;
+
+  const paymentToken = paymentTokens[0]?.toLowerCase();
+  const amount = amounts[0];
+  let royaltyRecipient;
+  let royaltyFeeEth;
+  let royaltyFeeUsd;
+
+  const paymentInEth = ethPaymentTokens.includes(paymentToken);
+
+  if (paymentInEth) {
+    salePrice = ethSalePrice = amount.toString() / 1e18;
+    usdSalePrice = ethSalePrice * event.price;
+  } else {
+    ({ salePrice, ethSalePrice, usdSalePrice, _ } =
+      await getHistoricalTokenPrice(event, paymentToken, amount));
+  }
+
+  return {
+    collection,
+    tokenId,
+    amount: 1,
+    salePrice,
+    ethSalePrice,
+    usdSalePrice,
+    paymentToken,
+    seller,
+    buyer,
+    royaltyRecipient,
+    royaltyFeeEth,
+    royaltyFeeUsd,
+  };
 };
 
 module.exports = { abi, config, parse };
