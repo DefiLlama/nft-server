1d8f754ba6522f48772416bda3e1391641e6953a
slasher125 <slasher125@protonmail.com>
2023-07-26 11:20:07 +0400

    optional exclusion of sales

--- src/api/controllers/transfers.js
+++ src/api/controllers/transfers.js
@@ -6,7 +6,7 @@ const { customHeaderFixedCache } = require('../../utils/customHeader');
 const { indexa } = require('../../utils/dbConnection');
 
 const getTransfers = async (req, res) => {
-  let { collectionId, tokenId } = req.query;
+  let { collectionId, tokenId, excludeSales } = req.query;
   if (!collectionId || !tokenId || !checkCollection(collectionId))
     return res.status(400).json('invalid collectionId/tokenId!');
 
@@ -16,30 +16,25 @@ const getTransfers = async (req, res) => {
   }
 
   const query = minify(`
-SELECT
-  encode(t.transaction_hash, 'hex') AS transaction_hash,
-  t.log_index,
-  t.block_time,
-  t.block_number,
-  encode(t.from_address, 'hex') AS from_address,
-  encode(t.to_address, 'hex') AS to_address,
-  t.amount
-FROM
-  ethereum.nft_transfers AS t
-WHERE
-  t.collection = $<collectionId>
-  AND t.token_id = $<tokenId>
-  AND NOT EXISTS (
-      SELECT
-          1
-      FROM
-          ethereum.nft_trades AS trades
-      WHERE
-          trades.collection = $<collectionId>
-          AND trades.token_id = $<tokenId>
-          AND trades.transaction_hash = t.transaction_hash
-  )
-  `);
+  SELECT
+    encode(t.transaction_hash, 'hex') AS transaction_hash,
+    t.log_index,
+    t.block_time,
+    t.block_number,
+    encode(t.from_address, 'hex') AS from_address,
+    encode(t.to_address, 'hex') AS to_address,
+    t.amount
+  FROM
+    ethereum.nft_transfers AS t
+  WHERE
+    t.collection = $<collectionId>
+    AND t.token_id = $<tokenId>
+    ${
+      excludeSales
+        ? 'AND NOT EXISTS (SELECT 1 FROM ethereum.nft_trades AS trades WHERE trades.collection = $<collectionId> AND trades.token_id = $<tokenId> AND trades.transaction_hash = t.transaction_hash)'
+        : ''
+    }
+    `);
 
   const response = await indexa.query(query, {
     collectionId: `\\${collectionId.slice(1)}`,
