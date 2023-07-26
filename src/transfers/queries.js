145eb02652a5868c5bfe08343b57105ac60d9b6b
slasher125 <slasher125@protonmail.com>
2023-07-26 10:02:34 +0400

    bug fix

--- src/transfers/queries.js
+++ src/transfers/queries.js
@@ -1,5 +1,7 @@
 const minify = require('pg-minify');
 
+const { pgp, indexa } = require('../utils/dbConnection');
+
 const queryTransfers = `
 SELECT
     encode(transaction_hash, 'hex') AS transaction_hash,
@@ -84,20 +86,15 @@ const insertTransfers = async (payload) => {
 };
 
 const deleteQ = `
-  DELETE FROM
-      ethereum.nft_transfers
-  WHERE
-      (
-          (
-              topic_0 = '\\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-              AND topic_3 IS NOT NULL
-          )
-          OR (
-              topic_0 = '\\xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62'
-          )
-      )
-      AND block_number >= $<startBlock>
-      AND block_number <= $<endBlock>
+DELETE FROM
+    ethereum.nft_transfers
+WHERE
+    (
+        topic_0 = '\\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+        OR topic_0 = '\\xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62'
+    )
+    AND block_number >= $<startBlock>
+    AND block_number <= $<endBlock>
   `;
 
 const deleteTransfers = async (startBlock, endBlock) => {
