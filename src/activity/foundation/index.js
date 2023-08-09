350c5f8fd9160efda33fea967574ce5e847849f7
slasher125 <slasher125@protonmail.com>
2023-08-09 18:02:52 +0400

    rename fields, add activity

--- src/activity/foundation/index.js
+++ src/activity/foundation/index.js
@@ -2,18 +2,24 @@ const abi = require('../../adapters/foundation/abi.json');
 const config = require('./config.json');
 
 const parse = (decodedData, event) => {
-  const { nftContract, tokenId, seller, price } = decodedData;
+  const { nftContract, tokenId, seller, price: _price } = decodedData;
 
-  const salePrice = price.toString() / 1e18;
+  const price = _price.toString() / 1e18;
 
   const collection = nftContract;
+  const address = seller;
+  const activity = config.events.find(
+    (e) => e.signatureHash === `0x${event.topic_0}`
+  )?.name;
+
   return {
     collection,
     tokenId,
-    salePrice,
-    ethSalePrice: salePrice,
-    usdSalePrice: salePrice * event.price,
-    seller,
+    price,
+    ethPrice: price,
+    usdPrice: price * event.price,
+    address,
+    activity,
   };
 };
 
