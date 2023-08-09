4a720493db9253ecaa7280981084cea1f220bda1
slasher125 <slasher125@protonmail.com>
2023-08-09 18:50:17 +0400

    more foundation events

--- src/activity/foundation/index.js
+++ src/activity/foundation/index.js
@@ -2,25 +2,56 @@ const abi = require('../../adapters/foundation/abi.json');
 const config = require('./config.json');
 
 const parse = (decodedData, event) => {
-  const { nftContract, tokenId, seller, price: _price } = decodedData;
-
-  const price = _price.toString() / 1e18;
-
-  const collection = nftContract;
-  const address = seller;
   const activity = config.events.find(
     (e) => e.signatureHash === `0x${event.topic_0}`
   )?.name;
 
-  return {
-    collection,
-    tokenId,
-    price,
-    ethPrice: price,
-    usdPrice: price * event.price,
-    address,
-    activity,
-  };
+  if (activity === 'BuyPriceSet') {
+    const { nftContract, tokenId, seller, price: _price } = decodedData;
+
+    const price = _price.toString() / 1e18;
+
+    return {
+      collection: nftContract,
+      tokenId,
+      price,
+      ethPrice: price,
+      usdPrice: price * event.price,
+      address: seller,
+      activity,
+    };
+  } else if (
+    ['BuyPriceCanceled', 'BuyPriceInvalidated', 'OfferInvalidated'].includes(
+      activity
+    )
+  ) {
+    const { nftContract, tokenId } = decodedData;
+
+    return {
+      collection: nftContract,
+      tokenId,
+      price: null,
+      ethPrice: null,
+      usdPrice: null,
+      address: null,
+      activity,
+    };
+  } else if (activity === 'OfferMade') {
+    const { nftContract, tokenId, buyer, amount, expiration } = decodedData;
+
+    const price = amount.toString() / 1e18;
+
+    return {
+      collection: nftContract,
+      tokenId,
+      price,
+      ethPrice: price,
+      usdPrice: price * event.price,
+      address: buyer,
+      activity,
+      expiration,
+    };
+  }
 };
 
 module.exports = { abi, config, parse };
