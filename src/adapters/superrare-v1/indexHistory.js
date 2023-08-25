61630fbb8b35ea3460499bb8c47276ef2a061886
slasher125 <slasher125@protonmail.com>
2023-08-25 19:18:26 +0400

    rename

--- src/adapters/superrare-v1/indexHistory.js
+++ src/adapters/superrare-v1/indexHistory.js
@@ -1,5 +1,3 @@
-const { stripZerosLeft } = require('ethers');
-
 const abi = require('./abi.json');
 const config = require('./config.json');
 
@@ -10,79 +8,34 @@ const parse = async (decodedData, event) => {
     (e) => e.signatureHash === `0x${event.topic_0}`
   )?.name;
 
-  if (['Bid', 'SetSalePrice', 'CancelBid'].includes(eventType)) {
-    const { _originContract, _bidder, _amount, _tokenId } = decodedData;
+  if (['Bid', 'CancelBid'].includes(eventType)) {
+    const { _bidder, _amount, _tokenId } = decodedData;
 
     const price = _amount.toString() / 1e18;
 
     return {
-      collection: _originContract,
+      collection: event.contract_address,
       tokenId: _tokenId,
       currencyAddress,
       price,
       ethPrice: price,
       usdPrice: price * event.price,
-      userAddress: _bidder ?? event.from_address,
-      eventType,
-    };
-  } else if (eventType === 'CancelAuction') {
-    const { _contractAddress, _tokenId, _auctionCreator } = decodedData;
-
-    return {
-      collection: _contractAddress,
-      tokenId: _tokenId,
-      userAddress: _auctionCreator,
-      eventType,
-    };
-  } else if (eventType === 'bidPH') {
-    const _originContract = stripZerosLeft(`0x${event.topic_1}`);
-    const _bidder = stripZerosLeft(`0x${event.topic_2}`);
-    const _tokenId = BigInt(`0x${event.topic_3}`);
-    const _amount = BigInt(`0x${event.data.slice(0, 64)}`);
-
-    const price = _amount.toString() / 1e18;
-
-    return {
-      collection: _originContract,
-      tokenId: _tokenId,
       userAddress: _bidder,
-      price,
-      ethPrice: price,
-      usdPrice: price * event.price,
       eventType,
     };
-  } else if (eventType === 'createColdieAuctionPH') {
-    const _originContract = stripZerosLeft(`0x${event.topic_1}`);
-    const _tokenId = BigInt(`0x${event.topic_2}`);
-    const userAddress = stripZerosLeft(`0x${event.topic_3}`);
-    const _amount = BigInt(`0x${event.data.slice(0, 64)}`);
+  } else if (eventType === 'SalePriceSet') {
+    const { _tokenId, _price } = decodedData;
 
-    const price = _amount.toString() / 1e18;
+    const price = _price.toString() / 1e18;
 
     return {
-      collection: _originContract,
+      collection: event.contract_address,
       tokenId: _tokenId,
-      userAddress,
+      currencyAddress,
       price,
       ethPrice: price,
       usdPrice: price * event.price,
-      eventType,
-    };
-  } else if (eventType === 'createScheduledAuctionPH') {
-    const _originContract = stripZerosLeft(`0x${event.topic_1}`);
-    const _tokenId = BigInt(`0x${event.topic_2}`);
-    const userAddress = stripZerosLeft(`0x${event.topic_3}`);
-    const _amount = BigInt(`0x${event.data.slice(64, 128)}`);
-
-    const price = _amount.toString() / 1e18;
-
-    return {
-      collection: _originContract,
-      tokenId: _tokenId,
-      userAddress,
-      price,
-      ethPrice: price,
-      usdPrice: price * event.price,
+      userAddress: event.from_address,
       eventType,
     };
   }
