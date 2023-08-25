9167700b8b927e5a3708225a25f9f45667511cec
slasher125 <slasher125@protonmail.com>
2023-08-25 12:20:08 +0400

    zora missing events r1

--- src/etype/history/superrare-v2/index.js
+++ src/etype/history/superrare-v2/index.js
@@ -1,37 +1,6 @@
 const abi = require('../../trades/superrare-v2/abi.json');
 const config = require('./config.json');
-const getHistoricalTokenPrice = require('../../../utils/price');
-
-const ethPaymentTokens = [
-  '0000000000000000000000000000000000000000',
-  'c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-].map((i) => `0x${i}`);
-
-const getPrice = async (currencyAddress, amount, event) => {
-  let price;
-  let ethPrice;
-  let usdPrice;
-
-  const paymentInEth = ethPaymentTokens.includes(
-    currencyAddress?.toLowerCase()
-  );
-
-  if (paymentInEth) {
-    price = ethPrice = amount.toString() / 1e18;
-    usdPrice = ethPrice * event.price;
-  } else {
-    const prices = await getHistoricalTokenPrice(
-      event,
-      currencyAddress,
-      amount
-    );
-    price = prices.salePrice;
-    ethPrice = prices.ethSalePrice;
-    usdPrice = prices.usdSalePrice;
-  }
-
-  return { price, ethPrice, usdPrice };
-};
+const getPrice = require('../../../utils/priceHistory');
 
 const parse = async (decodedData, event) => {
   const eventType = config.events.find(
