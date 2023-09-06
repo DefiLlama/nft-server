4dbc130b0399ddff8df5b2e788250754a50c2099
slasher125 <slasher125@protonmail.com>
2023-09-06 15:38:25 +0400

    rename queries, add rarible transfersingle

--- src/creator/queries.js
+++ src/creator/queries.js
@@ -25,7 +25,7 @@ WHERE
   AND e.block_number <= $<endBlock>
 `;
 
-const specific = `
+const specific1 = `
 SELECT
   encode(e.transaction_hash, 'hex') AS transaction_hash,
   e.log_index,
@@ -48,10 +48,35 @@ WHERE
   AND e.block_number <= $<endBlock>
 `;
 
+const specific2 = `
+SELECT
+  encode(e.transaction_hash, 'hex') AS transaction_hash,
+  e.log_index,
+  encode(e.contract_address, 'hex') AS contract_address,
+  encode(e.topic_0, 'hex') AS topic_0,
+  encode(e.topic_1, 'hex') AS topic_1,
+  encode(e.topic_2, 'hex') AS topic_2,
+  encode(e.topic_3, 'hex') AS topic_3,
+  encode(e.data, 'hex') AS data,
+  e.block_time,
+  e.block_number,
+  encode(e.block_hash, 'hex') AS block_hash
+FROM
+  ethereum.event_logs e
+WHERE
+  e.contract_address in ($<contractAddresses:csv>)
+  AND e.topic_0 in ($<eventSignatureHashes:csv>)
+  AND e.topic_2 = '\\x0000000000000000000000000000000000000000000000000000000000000000'
+  AND e.block_number >= $<startBlock>
+  AND e.block_number <= $<endBlock>
+`;
+
 const getEvents = async (task, startBlock, endBlock, config) => {
   const query =
-    config.version === 'rarible-transfer' || config.exchangeName === 'superrare'
-      ? specific
+    config.version === 'specific1'
+      ? specific1
+      : config.version === 'specific2'
+      ? specific2
       : generic;
 
   const eventSignatureHashes = config.events.map(
