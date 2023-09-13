22682b1ee8bcfb32cc819815264eab18ee901697
slasher125 <slasher125@protonmail.com>
2023-09-13 19:04:23 +0400

    ic

--- src/api/routes/history.js
+++ src/api/routes/history.js
@@ -4,5 +4,6 @@ const history = require('../controllers/history');
 
 router.route('/history').get(history.getHistory);
 router.route('/available').get(history.getAvailable);
+router.route('/available2').get(history.getAvailable2);
 
 module.exports = router;
