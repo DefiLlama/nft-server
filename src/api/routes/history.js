db1085526535416cd8e61ffefc1f0c7829aaf474
slasher125 <slasher125@protonmail.com>
2023-09-18 15:15:46 +0400

    combine available

--- src/api/routes/history.js
+++ src/api/routes/history.js
@@ -4,6 +4,5 @@ const history = require('../controllers/history');
 
 router.route('/history').get(history.getHistory);
 router.route('/available').get(history.getAvailable);
-router.route('/available2').get(history.getAvailable2);
 
 module.exports = router;
