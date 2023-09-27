cb70eaa880d0be9987d0ef6f75e4837e505526fe
slasher125 <slasher125@protonmail.com>
2023-09-27 17:39:09 +0400

    creator bug fix in case of shared collection and non existent token_id

--- src/api/controllers/creator.js
+++ src/api/controllers/creator.js
@@ -137,6 +137,7 @@ nfts(collection, token_id) AS (
     VALUES
         ${values}
 ),
+-- any collections we do not directly track via adapters (eg missing factories as well as sovereign)
 remaining AS (
     SELECT
         *
@@ -162,11 +163,32 @@ SELECT
     encode(transaction_from_address, 'hex') AS creator
 FROM
     remaining
+WHERE
+    address NOT IN ($<sharedCollections:csv>)
     `);
 
   const response = await indexa.query(query, {
     values,
     collections: nfts.map((nft) => `\\${nft.split(':')[0].slice(1)}`),
+    sharedCollections: [
+      '0x2963ba471e265e5f51cafafca78310fe87f8e6d1',
+      '0x2a46f2ffd99e19a89476e2f62270e0a35bbf0756',
+      '0x2d9e5de7d36f3830c010a28b29b3bdf5ca73198e',
+      '0x3b3ee1931dc30c1957379fac9aba94d1c48a5405',
+      '0x41a322b28d0ff354040e2cbc676f0320d8c8850d',
+      '0x495f947276749ce646f68ac8c248420045cb7b5e',
+      '0x60f80121c31a0d46b5279700f9df786054aa5ee5',
+      '0x6a5ff3ceecae9ceb96e6ac6c76b82af8b39f0eb3',
+      '0xabb3738f04dc2ec20f4ae4462c3d069d02ae045b',
+      '0xabefbc9fd2f806065b4f3c237d4b59d9a97bcac7',
+      '0xb66a603f4cfe17e3d27b87a8bfcad319856518b8',
+      '0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0',
+      '0xc9154424b823b10579895ccbe442d41b9abd96ed',
+      '0xcd51b81ac1572707b7f3051aa97a31e2afb27d45',
+      '0xd07dc4262bcdbf85190c01c996b4c06a461d2430',
+      '0xf6793da657495ffeff9ee6350824910abc21356c',
+      '0xfbeef911dc5821886e1dda71586d90ed28174b7d',
+    ].map((c) => `\\${c.slice(1)}`),
   });
 
   if (!response) {
