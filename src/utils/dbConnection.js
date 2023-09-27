9e2ccb0b8f67eb8ea9f6fad1d0077c72277ef768
slasher125 <slasher125@protonmail.com>
2023-09-27 19:09:01 +0400

    increase db connections

--- src/utils/dbConnection.js
+++ src/utils/dbConnection.js
@@ -9,7 +9,7 @@ pgp.pg.types.setTypeParser(1700, parseFloat);
 
 const indexa = pgp({
   connectionString: process.env.DATABASE_URL_INDEXA,
-  max: 10,
+  max: 50,
   idleTimeoutMillis: 30000,
 });
 
