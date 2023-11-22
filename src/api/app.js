a1d1039519abe65cdf07e1fd710922a3726f957c
slasher125 <slasher125@protonmail.com>
2023-11-22 19:44:41 +0400

    temp change api logger

--- src/api/app.js
+++ src/api/app.js
@@ -10,7 +10,9 @@ const history = require('./routes/history');
 const creator = require('./routes/creator');
 
 const app = express();
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
+app.use(morgan(':method :url :status :response-time ms - :remote-addr'));
+
 app.use(helmet());
 
 app.use('/', [trades, floor, orderbook, transfers, history, creator]);
